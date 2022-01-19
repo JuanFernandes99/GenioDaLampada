01cde735aa68fab150ce3ea59f477b63ba775b20
jbarrinhos <96847584+jbarrinhos@users.noreply.github.com>
2022-01-19 22:45:59 +0000

    genios_atualizado
    adição das classes génios

--- src/genioLampada/BemHumorado.java
+++ src/genioLampada/BemHumorado.java
@@ -2,4 +2,19 @@ package genioLampada;
 
 public class BemHumorado extends Genio {
 
+	protected int desejoDisponivel;
+	protected int auxiliarGenio;
+
+	public BemHumorado(int max) {
+		super(max);
+	}
+
+	public void conceDedesejo() {
+		// return
+	}
+
+	public int desejoDisponivel() {
+		return getMax() - getDesejosConcedidos();
+	}
+
 }
--- src/genioLampada/MalHumorado.java
+++ src/genioLampada/MalHumorado.java
@@ -2,4 +2,8 @@ package genioLampada;
 
 public class MalHumorado extends Genio {
 
+	public MalHumorado(int max) {
+		super(1);
+
+	}
 }
