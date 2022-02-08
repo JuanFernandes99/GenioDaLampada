93243d61b2b871a0976d27552dbc34d00814a676
studentsara <97111715+studentsara@users.noreply.github.com>
2022-02-08 13:02:42 +0000

    s

--- src/genioLampada/Main.java
+++ src/genioLampada/Main.java
@@ -8,37 +8,104 @@ public class Main {
 	static Lampada nlampada = new Lampada(randomNum);
 
 	public static void main(String[] args) {
-		System.out.println("Bem-vindo ao jogo da Lampada MÃ¡gica!");
+		System.out.println("Bem-vindo ao jogo da Lampada Mágica!");
 		while (true) {
 			menu();
 		}
 	}
+	
+	
+	
+	
+	
+/*
+ * public static void esfregarLampada() {
 
+		System.out.println("Quantas vezes deseja esfregar a lâmpada?");
+
+		int esfregadelas = sc.nextInt();
+
+		Lampada.esfregar(esfregadelas);
+
+		if (esfregadelas <= 0) {
+
+			System.out.println("\n numero tem de ser superior a 0");
+
+			esfregarLampada();
+
+		}
+
+		else if (esfregadelas != 0) {
+
+			int esfregadelasAcumuladas = Lampada.quantidadeEsfregadelas();
+			System.out.println("Esfregou a lâmpada: " + esfregadelasAcumuladas + "vezes");
+
+			int aux = pedirDesejos();
+
+			if (esfregadelas % 2 == 0) {
+
+				BemHumorado gBemHumorado = new BemHumorado();
+				gBemHumorado.concedeDesejo(aux);
+
+			} else {
+
+				MalHumorado gmauHumorado = new MalHumorado();
+				gmauHumorado.concedeDesejo(aux);
+			}
+
+			Lampada.setgeniosDisponiveis();
+		}
+
+	}
+
+ * 
+ * 
+ * 
+ * */
+	
+	
+	
+	
+	
+	
 	public static void menu() {
 
-		System.out.println("\nEscolha uma das opÃ§Ãµes disponÃ­veis:");
-		System.out.println("1 - Esfregar a LÃ¢mpada");
-		System.out.println("2 - Sair do Jogo");
+		System.out.println("\nEscolha uma das opções disponíveis:");
+		System.out.println("1 - Instruções do Jogo");
+		System.out.println("2 - Esfregar a Lâmpada");
+		System.out.println("3 - Sair do Jogo");
 
 		int opcao = sc.nextInt();
 		switch (opcao) {
-		case 1:
-			if (Lampada.getGeniosDisponiveis() > 0) {
-				esfregarLampada();
-			} else {
-				DemonioReciclavel.menuDemonio();
-			}
-			/*
-			 * caso nao seja, chamar o demonio, abrir menu que pergunta se Ã© para garantir
-			 * desejo ou recarregar lampada. Se for p/ recarregar, chamar funcao que estÃ¡ na
-			 * lampada atualizar o numero de genios disponiveis com o nÃºmero aleatorio dado
-			 * na criaÃ§ao da lampada
-			 */
+		
+			case 1:
+				System.out.println(" Instruções\n" + "\n"
+						+ "Este jogo realiza-se dentro de uma Lâmpada Mágica que contêm génios indeterminados. \nO objetivo é iniciar a lâmpada, chamar génios e pedir desejos, que determinam se irá sair um génio bem-humorado ou mal-humorado.\nTenta a tua sorte e vê o que acontece no final!\n"
+						+ "\n" + "Passos:\n" + "\n" + "1. O jogador tem de iniciar a lâmpada;\n"
+						+ "2. O jogador decide quantas vezes quer esfregar a lâmpada;\n"
+						+ "3. O jogador pede quantos desejos quer ver realizados;\n" + "4. BOOOM!!! Sai o génio;\n"
+						+ "\nA sequência será repetida pelo jogador do ponto 2 até ao ponto 4.\n" + "Atenção!!!\n"
+						+ "\nO jogador decide quando acabar o jogo a qualquer momento através da opção disponível “sair do jogo”;\n");
+			break;
+		
+		
+			case 2:
+				if (Lampada.getGeniosDisponiveis() > 0) {
+					esfregarLampada();
+				} else {
+					DemonioReciclavel.menuDemonio();
+				}
+				/*
+				 * caso nao seja, chamar o demonio, abrir menu que pergunta se Ã© para garantir
+				 * desejo ou recarregar lampada. Se for p/ recarregar, chamar funcao que estÃ¡ na
+				 * lampada atualizar o numero de genios disponiveis com o nÃºmero aleatorio dado
+				 * na criaÃ§ao da lampada
+				 */
 			break;
 
-		case 2:
-			System.out.println("2 - Sair");
-			System.exit(0);
+			case 3:
+				System.out.println("2 - Sair");
+				System.exit(0);
 
 			break;
 		}
@@ -46,26 +113,42 @@ public class Main {
 	}
 
 	public static void esfregarLampada() {
+		
+		System.out.println("Quantas vezes deseja esfregar a lâmpada?");
 
-		System.out.println("Quantas vezes deseja esfregar a lÃ¢mpada?");
 		int esfregadelas = sc.nextInt();
+
 		Lampada.esfregar(esfregadelas);
-		int esfregadelasAcumuladas = Lampada.quantidadeEsfregadelas();
-		System.out.println("Esfregou a lÃ¢mpada: " + esfregadelasAcumuladas + " vezes");
 
-		int aux = Genio.pedirDesejos();
+		if (esfregadelas <= 0) {
 
-		if (esfregadelas % 2 == 0) {
+			System.out.println("\nO número deve ser superior a zero.");
 
-			BemHumorado gBemHumorado = new BemHumorado();
-			gBemHumorado.concedeDesejo(aux);
+			esfregarLampada();
 
-		} else {
-
-			MalHumorado gmauHumorado = new MalHumorado();
-			gmauHumorado.concedeDesejo(aux);
 		}
-		Lampada.setgeniosDisponiveis();
+
+		else if (esfregadelas != 0) {
+
+			int esfregadelasAcumuladas = Lampada.quantidadeEsfregadelas();
+			System.out.println("Esfregou a lâmpada: " + esfregadelasAcumuladas + "vezes");
+
+			int aux = Genio.pedirDesejos();
+
+			if (esfregadelas % 2 == 0) {
+
+				BemHumorado gBemHumorado = new BemHumorado();
+				gBemHumorado.concedeDesejo(aux);
+
+			} else {
+
+				MalHumorado gmauHumorado = new MalHumorado();
+				gmauHumorado.concedeDesejo(aux);
+			}
+
+			Lampada.setgeniosDisponiveis();
+
+		}
 	}
 
 }
