9bbb2d120ec51bdddc5bacfecc064b9906af94e4
studentsara <97111715+studentsara@users.noreply.github.com>
2022-02-12 13:29:56 +0000

    ort

--- src/genioLampada/GBemHumorado.java
+++ src/genioLampada/GBemHumorado.java
@@ -10,11 +10,11 @@ public class GBemHumorado extends Genio {
 	}
 
 	// FuncÃ£o concedeDesejo 
-	// Concede desejos, permitindo ao código ser executado repetitivamente baseado
-	// na condição da auxiliar ser menor comparativamente ao máximo de desejos
+	// Concede desejos, permitindo ao cï¿½digo ser executado repetitivamente baseado
+	// na condiï¿½ï¿½o da auxiliar ser menor comparativamente ao mï¿½ximo de desejos
 	// (atributo) e faz a contagem dos desejos pedidos.
 	public void concedeDesejo(int maxDesejos) {
-		System.out.println("Ola¡, eu sou o genio bem-humorado e lhe concederei todos os desejos que voce me pedir");
+		System.out.println("Olaï¿½, eu sou o genio bem-humorado e vou conceder todos os desejos que voce me pedir.");
 		super.concedeDesejo(maxDesejos);
 	}
 
