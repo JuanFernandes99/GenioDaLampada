c1ee8b719f202283d2a640ca3f142ffabdffbf87
JuanFernandes99 <juandanieldosr@gmail.com>
2022-03-03 17:54:06 +0000

    Update GMalHumorado.java

--- src/genioLampada/GMalHumorado.java
+++ src/genioLampada/GMalHumorado.java
@@ -19,4 +19,12 @@ public class GMalHumorado extends Genio {
 		System.out.println("Desejo: (" + desejoIndicado + "), Realizado! :)");
 		System.out.print("Esgotou os seus desejos :( , ");
 	}
+	public void concedeDesejoo(int maxDesejos) {
+		System.out.println(
+				"Arggggg Quem se atreveu a me chamar? Sou o Scar e estou de mau humor!!! Tens direito a UM desejo");
+		System.out.println("\nIndique o seu desejo");
+		String desejoIndicado = Main.sc.nextLine();
+		System.out.println("Desejo: (" + desejoIndicado + "), Realizado! :)");
+		System.out.print("Esgotou os seus desejos :( , ");
+	}
 }
